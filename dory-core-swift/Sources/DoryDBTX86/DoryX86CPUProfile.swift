c811a7b2a8a4ebe85ab742d4553c8654e7034e17
Augustus Otu <Augani7@gmail.com>
2026-08-31 01:21:55 +0000

    feat: advertise qualified x86 simd baseline

--- dory-core-swift/Sources/DoryDBTX86/DoryX86CPUProfile.swift
+++ dory-core-swift/Sources/DoryDBTX86/DoryX86CPUProfile.swift
@@ -72,8 +72,8 @@ public struct DoryX86CPUProfile: Codable, Sendable, Hashable {
   public static let compatibleV1 = Self(
     identifier: compatibleV1Identifier,
     features: [
-      .tsc, .msr, .cmpxchg8b, .apic, .cmov, .cmpxchg16b, .syscall, .executeDisable, .longMode,
-      .invariantTSC,
+      .x87, .tsc, .msr, .cmpxchg8b, .apic, .cmov, .mmx, .fxsave, .sse, .sse2, .cmpxchg16b,
+      .syscall, .executeDisable, .longMode, .invariantTSC,
     ],
     physicalAddressBits: 40,
     linearAddressBits: 48,
