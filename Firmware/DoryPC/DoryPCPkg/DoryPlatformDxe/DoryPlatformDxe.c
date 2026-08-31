285413dd8f8e09f1fcdd55a1a9639a43c5a7a66a
Augustus Otu <Augani7@gmail.com>
2026-08-31 10:32:49 +0000

    fix: publish dorypc platform dependencies

--- Firmware/DoryPC/DoryPCPkg/DoryPlatformDxe/DoryPlatformDxe.c
+++ Firmware/DoryPC/DoryPCPkg/DoryPlatformDxe/DoryPlatformDxe.c
@@ -7,6 +7,7 @@
 #include <Library/IoLib.h>
 #include <Library/PcdLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Protocol/MpInitLibDepProtocols.h>
 
 EFI_STATUS
 EFIAPI
@@ -17,13 +18,21 @@ DoryPlatformDxeEntryPoint (
 {
   EFI_STATUS Status;
   UINTN      Base;
+  UINT32     CpuCount;
+  EFI_GUID   *MpInitLibDepProtocol;
   UINT64     Rsdp;
   UINT64     Smbios;
 
   Base = (UINTN)FixedPcdGet64 (PcdFirmwareConfigurationBase);
   if ((MmioRead64 (Base + DORY_PC_CONFIGURATION_MAGIC_OFFSET) != DORY_PC_CONFIGURATION_MAGIC) ||
-      (MmioRead32 (Base + DORY_PC_CONFIGURATION_VERSION_OFFSET) != DORY_PC_CONFIGURATION_VERSION))
+      (MmioRead32 (Base + DORY_PC_CONFIGURATION_VERSION_OFFSET) != DORY_PC_CONFIGURATION_VERSION) ||
+      (MmioRead32 (Base + DORY_PC_CONFIGURATION_HEADER_SIZE_OFFSET) < DORY_PC_CONFIGURATION_HEADER_SIZE))
   {
+    return EFI_COMPROMISED_DATA;
+  }
+
+  CpuCount = MmioRead32 (Base + DORY_PC_CONFIGURATION_CPU_COUNT_OFFSET);
+  if ((CpuCount == 0) || (CpuCount > 255)) {
     return EFI_COMPROMISED_DATA;
   }
 
@@ -33,5 +42,21 @@ DoryPlatformDxeEntryPoint (
   if (EFI_ERROR (Status)) {
     return Status;
   }
-  return gBS->InstallConfigurationTable (&gEfiSmbios3TableGuid, (VOID *)(UINTN)Smbios);
+
+  Status = gBS->InstallConfigurationTable (&gEfiSmbios3TableGuid, (VOID *)(UINTN)Smbios);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  MpInitLibDepProtocol = (CpuCount == 1) ?
+                         &gEfiMpInitLibUpDepProtocolGuid :
+                         &gEfiMpInitLibMpDepProtocolGuid;
+  return gBS->InstallMultipleProtocolInterfaces (
+                &ImageHandle,
+                MpInitLibDepProtocol,
+                NULL,
+                &gIoMmuAbsentProtocolGuid,
+                NULL,
+                NULL
+                );
 }
