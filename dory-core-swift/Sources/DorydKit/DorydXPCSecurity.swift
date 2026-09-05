73612cbc086052ea2ac0f437afa1a37f43b169fb
Augustus Otu <Augani7@gmail.com>
2026-09-05 02:24:39 +0000

    Fix dynamic daemon signature validation flags

--- dory-core-swift/Sources/DorydKit/DorydXPCSecurity.swift
+++ dory-core-swift/Sources/DorydKit/DorydXPCSecurity.swift
@@ -93,7 +93,7 @@ public enum DorydXPCSecurity {
         }
         return SecCodeCheckValidity(
             code,
-            SecCSFlags(rawValue: kSecCSCheckAllArchitectures),
+            SecCSFlags(),
             requirement
         ) == errSecSuccess
     }
