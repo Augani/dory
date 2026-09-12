341ec093c2f6dcde14811309ae7ad27a81a3a7fe
Augustus Otu <Augani7@gmail.com>
2026-09-12 06:48:32 +0000

    perf(x86): combine tier1 direct chains with ibtc

--- dory-core-swift/Sources/DoryMachinePC/DoryPCDirectKernelMachine.swift
+++ dory-core-swift/Sources/DoryMachinePC/DoryPCDirectKernelMachine.swift
@@ -633,7 +633,8 @@ public final class DoryPCDirectKernelMachine: @unchecked Sendable {
     baselineJITMaximumCodeBytes: Int = DoryARM64BaselineExecutor.defaultMaximumCodeBytes,
     baselineJITTier1Enabled: Bool = true,
     baselineJITRawTargetPredictionOptions: DoryARM64RawTargetPredictionOptions = [
-      .tier1DirectChain
+      .tier1DirectChain,
+      .indirectBranchTargetCache
     ],
     optimizingJITWarmupDispatches: UInt8 = 8,
     clockSource: DoryPCClockSource = .deterministic,
